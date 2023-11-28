4a0ab039a45bb69b50e0e445aca2147d8beed9ab
The2Losers <prnvinformal@gmail.com>
2023-11-29 02:28:15 +0530

    Final draft i guess

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/firstdraft" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
